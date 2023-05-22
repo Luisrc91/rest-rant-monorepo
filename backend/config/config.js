e60739e9d10423188d72a8a5015fdb7c766ecdca
Luis <luisrc91@live.com>
2023-05-21 23:09:01 -0400

    added password form

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,20 +5,20 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": "127.0.0.1",    
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
