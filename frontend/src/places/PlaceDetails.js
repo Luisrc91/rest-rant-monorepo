ee9d010a411b05424a8df3d90e8c6a25d303f5ab
Luis <luisrc91@live.com>
2023-05-24 21:29:23 -0400

    authentication for comments

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router"
-import CommentCard from './CommentCard'
+import { useEffect, useState, useContext } from "react";
+import { useHistory, useParams } from "react-router";
+import CommentCard from "./CommentCard";
 import NewCommentForm from "./NewCommentForm";
+import { CurrentUser } from "../contexts/CurrentUser";
+
 
 function PlaceDetails() {
 
 	const { placeId } = useParams()
 
 	const history = useHistory()
+
+	const { currentUser } = useContext(CurrentUser)
 
 	const [place, setPlace] = useState(null)
 
@@ -51,7 +55,8 @@ function PlaceDetails() {
 		const response = await fetch(`http://localhost:5000/places/${place.placeId}/comments`, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json'
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${localStorage.getItem('token')}`
 			},
 			body: JSON.stringify(commentAttributes)
 		})
@@ -101,54 +106,48 @@ function PlaceDetails() {
 		})
 	}
 
-
-	return (
-		<main>
-			<div className="row">
-				<div className="col-sm-6">
-					<img style={{ maxWidth: 200 }} src={place.pic} alt={place.name} />
-					<h3>
-						Located in {place.city}, {place.state}
-					</h3>
-				</div>
-				<div className="col-sm-6">
-					<h1>{place.name}</h1>
-					<h2>
-						Rating
-					</h2>
-					{rating}
-					<br />
-					<h2>
-						Description
-					</h2>
-					<h3>
-						{place.name} has been serving {place.city}, {place.state} since {place.founded}.
-					</h3>
-					<h4>
-						Serving {place.cuisines}.
-					</h4>
-					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
-				</div>
-			</div>
-			<hr />
-			<h2>Comments</h2>
-			<div className="row">
-				{comments}
-			</div>
-			<hr />
-			<h2>Got Your Own Rant or Rave?</h2>
-			<NewCommentForm
-				place={place}
-				onSubmit={createComment}
-			/>
-		</main>
-	)
+  return (
+    <main>
+      <div className="row">
+        <div className="col-sm-6">
+          <img style={{ maxWidth: 200 }} src={place.pic} alt={place.name} />
+          <h3>
+            Located in {place.city}, {place.state}
+          </h3>
+        </div>
+        <div className="col-sm-6">
+          <h1>{place.name}</h1>
+          <h2>Rating</h2>
+          {rating}
+          <br />
+          <h2>Description</h2>
+          <h3>
+            {place.name} has been serving {place.city}, {place.state} since{" "}
+            {place.founded}.
+          </h3>
+          <h4>Serving {place.cuisines}.</h4>
+          <br />
+          <a className="btn btn-warning" onClick={editPlace}>
+            Edit
+          </a>
+          {` `}
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onClick={deletePlace}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+      <hr />
+      <h2>Comments</h2>
+      <div className="row">{comments}</div>
+      <hr />
+      <h2>Got Your Own Rant or Rave?</h2>
+      <NewCommentForm place={place} onSubmit={createComment} />
+    </main>
+  );
 }
 
-export default PlaceDetails
+export default PlaceDetails;
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,15 +15,16 @@ function LoginForm() {
   const [errorMessage, setErrorMessage] = useState(null);
 
   async function handleSubmit(e) {
-    e.preventDefault();
-
+    e.preventDefault()
+    
     const response = await fetch(`http://localhost:5000/authentication/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+
 
     const data = await response.json();
 
