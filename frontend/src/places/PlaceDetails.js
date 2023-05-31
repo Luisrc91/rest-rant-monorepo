2bd886c73bb1c4124f253e27aaa56b31e0c21f5c
Luis <luisrc91@live.com>
2023-05-31 01:05:08 -0400

    finish

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,15 +1,17 @@
 import { useEffect, useState, useContext } from "react";
-import { useHistory, useParams } from "react-router";
-import CommentCard from "./CommentCard";
-import NewCommentForm from "./NewCommentForm";
+import { useHistory, useParams } from "react-router"
 import { CurrentUser } from "../contexts/CurrentUser";
-
+import CommentCard from './CommentCard'
+import NewCommentForm from "./NewCommentForm";
 
 function PlaceDetails() {
 
 	const { placeId } = useParams()
+
 	const history = useHistory()
-	// const { currentUser } = useContext(CurrentUser)
+
+	const { currentUser } = useContext(CurrentUser)
+
 	const [place, setPlace] = useState(null)
 
 	useEffect(() => {
@@ -52,8 +54,8 @@ function PlaceDetails() {
 		const response = await fetch(`http://localhost:5000/places/${place.placeId}/comments`, {
 			method: 'POST',
 			headers: {
-				'Authorization': `Bearer ${localStorage.getItem('token')}`,
 				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${localStorage.getItem('token')}`
 			},
 			body: JSON.stringify(commentAttributes)
 		})
@@ -103,48 +105,64 @@ function PlaceDetails() {
 		})
 	}
 
-  return (
-    <main>
-      <div className="row">
-        <div className="col-sm-6">
-          <img style={{ maxWidth: 200 }} src={place.pic} alt={place.name} />
-          <h3>
-            Located in {place.city}, {place.state}
-          </h3>
-        </div>
-        <div className="col-sm-6">
-          <h1>{place.name}</h1>
-          <h2>Rating</h2>
-          {rating}
-          <br />
-          <h2>Description</h2>
-          <h3>
-            {place.name} has been serving {place.city}, {place.state} since{" "}
-            {place.founded}.
-          </h3>
-          <h4>Serving {place.cuisines}.</h4>
-          <br />
-          <a className="btn btn-warning" onClick={editPlace}>
-            Edit
-          </a>
-          {` `}
-          <button
-            type="submit"
-            className="btn btn-danger"
-            onClick={deletePlace}
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-      <hr />
-      <h2>Comments</h2>
-      <div className="row">{comments}</div>
-      <hr />
-      <h2>Got Your Own Rant or Rave?</h2>
-      <NewCommentForm place={place} onSubmit={createComment} />
-    </main>
-  );
+	let placeActions = null
+
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>{` `}
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+
+
+	return (
+		<main>
+			<div className="row">
+				<div className="col-sm-6">
+					<img style={{ maxWidth: 200 }} src={place.pic} alt={place.name} />
+					<h3>
+						Located in {place.city}, {place.state}
+					</h3>
+				</div>
+				<div className="col-sm-6">
+					<h1>{place.name}</h1>
+					<h2>
+						Rating
+					</h2>
+					{rating}
+					<br />
+					<h2>
+						Description
+					</h2>
+					<h3>
+						{place.name} has been serving {place.city}, {place.state} since {place.founded}.
+					</h3>
+					<h4>
+						Serving {place.cuisines}.
+					</h4>
+					<br />
+					{placeActions}
+				</div>
+			</div>
+			<hr />
+			<h2>Comments</h2>
+			<div className="row">
+				{comments}
+			</div>
+			<hr />
+			<h2>Got Your Own Rant or Rave?</h2>
+			<NewCommentForm
+				place={place}
+				onSubmit={createComment}
+			/>
+		</main>
+	)
 }
 
-export default PlaceDetails;
+export default PlaceDetails
